23fa700afa8084d540b4b0f777e5ee5f99477d62
Vrabie-Alin-Stefan <alinvs09@gmail.com>
2019-06-11 18:17:29 +0300

    1

---
+++ mvc/app/models/DBconn.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost";
+$ServerUsername = "root";
+$ServerPassword = "";
+$dbname = "project";
+
+$conn = mysqli_connect($servername, $ServerUsername, $ServerPassword, $dbname);
---
+++ mvc/app/models/TableService.php
@@ -0,0 +1,15 @@
+<?php
+
+class TableService
+{
+    public function __construct()
+    {
+        include('../app/models/con.php');
+        //require_once '../con.php';
+    }
+
+    public function InsertTable($masa)
+    {
+
+    }
+}
